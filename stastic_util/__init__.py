99dd1f6a3259aa861e1d9fe19741b1a503c05d01
taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2022-03-15 16:17:44 +0900

    Delete stastic_util directory

--- stastic_util/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import config, log
-
-__author__ = "Taitaitatata (TEAM Nekozouneko)"
-__copyright__ = "Copyright (C) 2022 TEAM Nekozouneko"
-__license__ = "GNU General Public License (GNU GPLv3)"
-__repository__ = "https://github.com/TEAMNekozouneko/StasticBot"
-__version__ = "1.0.0-alpha1"
