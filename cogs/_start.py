1f3c4d6715bdfb1dc804cdd658ba5d6d635ff40d
Taitaitatata <taitaitatatasub@outlook.jp>
2022-03-15 18:49:25 +0900

    [Update] 起動時のコグを実装

---
+++ cogs/_start.py
@@ -0,0 +1,32 @@
+import asyncio
+import discord, sys
+
+sys.path.append("../")
+
+from discord.ext import commands
+
+from statistic_util import *
+
+class _start(commands.Cog):
+    
+    def __init__(self, bot: discord.Bot):
+        self.bot = bot
+        self._last_member = None
+    
+    @commands.Cog.listener()
+    async def on_connect(self):
+        log.info("Discordに接続中...")
+    
+    @commands.Cog.listener()
+    async def on_ready(self):
+        log.info("接続しました。")
+
+        log.info("設定を読み込み中...")
+        self.cfg = config.loadConfig()
+        log.info("読み込みました。")
+
+        log.info("コンソールを起動しています...")
+        console_task = asyncio.create_task(console.start())
+
+def setup(bot: discord.Bot):
+    bot.add_cog(_start(bot))
