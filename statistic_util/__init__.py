3f86e82c28adbd88d35707e026f7c8fe03e06160
Taitaitatata <taitaitatatasub@outlook.jp>
2022-03-16 15:54:06 +0900

    [Update] いらないのにモジュール説明追加

--- statistic_util/__init__.py
+++ statistic_util/__init__.py
@@ -1,3 +1,13 @@
+"""
+## Statistic Util Module
+
+モジュールあるのに使えないアホ向けの関数群や
+あるとちょっと便利なもの軍です。
+
+:copyright: (C) 2022 TEAM Nekozouneko
+:license: GNU GPLv3, ./LICENSEや`__license__`を見よう
+"""
+
 from .config import config
 from .console import console
 from .log import log
@@ -6,4 +16,5 @@ __author__ = "Taitaitatata (TEAM Nekozouneko)"
 __copyright__ = "Copyright 2022 TEAM Nekozouneko"
 __license__ = "GNU General Public License (GNU GPLv3)"
 __repository__ = "https://github.com/TEAMNekozouneko/StasticBot"
+__title__ = "Statisitc Util"
 __version__ = "1.0.0-rc1"
