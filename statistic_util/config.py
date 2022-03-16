784b9ec6492bc5b5ea7af7a5d560e09911323596
Taitaitatata <taitaitatatasub@outlook.jp>
2022-03-16 15:54:37 +0900

    [Update] 関数説明を追加

--- statistic_util/config.py
+++ statistic_util/config.py
@@ -1,28 +1,33 @@
 import json
 
-class config:
-    
+class config:    
 
+    # 初期の設定ファイルの内容です。
     defaultConfig = {
         "token": "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
 
         "srvId": "000000000000000"
     }
 
+    # 読み込んだ設定ファイルの内容
     loadedConfig = None
 
     @classmethod
-    def reloadConfig(self):
-        return self.loadConfig()
+    def reloadConfig(self, fp: str = "config.json"):
+        # reloadConfig()の存在異議とは??????????????
+        """設定ファイルを再読み込みします。"""
+        return self.loadConfig(fp)
 
     @classmethod
-    def loadConfig(self) -> dict:
-        with open("config.json", "r") as conf:
+    def loadConfig(self, fp: str = "config.json") -> dict:
+        """設定ファイルを読み込みます"""
+        with open(fp, "r") as conf:
             loadedConfig = json.load(conf)
         return loadedConfig
 
     @classmethod
-    def saveDefaultConfig(self):
-        with open("config.json", "w") as conf:
+    def saveDefaultConfig(self, fp: str = "config.json"):
+        """設定ファイルをリセットします。"""
+        with open(fp, "w") as conf:
             json.dump(self.defaultConfig, conf)
         return
