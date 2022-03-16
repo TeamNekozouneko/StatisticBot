009439d0dedea7b00c3c1392a8bd2ef0ff391ba4
Taitaitatata <taitaitatatasub@outlook.jp>
2022-03-16 15:55:32 +0900

    [Update] コンソールのシステムを改良

--- statistic_util/console.py
+++ statistic_util/console.py
@@ -10,23 +10,33 @@ class console:
     pattern = [
         {
             "aliases": ["?"],
-            "cmd": log.info,
+            "output": [
+                "help - コマンドのヘルプを表示",
+                "stop - ボットを停止します。"
+            ],
+            "cmd": None,
             "name": "help"
         },
         {
             "aliases": ["exit", "alt+f4"],
             "cmd": sys.exit,
+            "output": None,
             "name": "stop"
         }
     ]
     
     @classmethod
     async def start(self):
+        """コンソールを起動します。"""
         log.info("コンソールを起動しました。")
         cs = await aioconsole.ainput("~ $ ")
 
         while True:
             for pt in self.pattern:
                 if (pt["name"] == cs) or (cs in pt["aliases"]):
-                    r = pt["cmd"]("help - ヘルプを表示")
+                    if (not pt["output"] is None):
+                        for out in pt["output"]:
+                            log.info(out)
+                    if (not pt["cmd"] is None):
+                        pt["cmd"]()
             cs = await aioconsole.ainput("~ $ ")
