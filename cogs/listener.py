26fea6107b9c9aa0329f8b3edcb0fb4a86acd4c7
Taitaitatata <taitaitatatasub@outlook.jp>
2022-03-18 17:19:53 +0900

    [Update] データベースにはこれからサーバーIDを追加するように変更

--- cogs/listener.py
+++ cogs/listener.py
@@ -23,15 +23,19 @@ class msgListener(commands.Cog):
         
         tableName = f"{self.cfg['database']['prefix']}message"
 
-        self.db.create_table(tableName, "author_id int, message_content str, message_id int")
+        self.db.create_table(tableName, "author_id int, message_content str, message_id int, guild_id int")
 
-        self.db.insert(tableName, (message.author.id, message.content, message.id))
+        self.db.insert(tableName, (message.author.id, message.content, message.id, message.guild.id))
     
     @commands.Cog.listener()
     async def on_message_delete(self, message: discord.Message):
-        print("delete Process")
         tableName = f"{self.cfg['database']['prefix']}message"
         self.db.execute(f"DELETE FROM {tableName} WHERE author_id == {message.author.id} AND message_id == {message.id}")
+    
+    @commands.Cog.listener()
+    async def on_message_edit(self, before: discord.Message, after: discord.Message):
+        tableName = f"{self.cfg['database']['prefix']}message"
+        self.db.execute(f"UPDATE {tableName} SET message_content = \"{after.content}\" WHERE author_id == {after.author.id} AND message_id == {after.id}")
 
 def setup(bot: discord.Bot):
     bot.add_cog(msgListener(bot))
