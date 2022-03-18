3d4fc59effff6f8f8519335b926cc1f79093f85e
Taitaitatata <taitaitatatasub@outlook.jp>
2022-03-18 17:21:09 +0900

    [Update] コマンドパターンをループ開始の度に設定するように変更

--- statistic_util/Console.py
+++ statistic_util/Console.py
@@ -3,38 +3,52 @@
 """
 import aioconsole, sys
 
+from . import dblib, config
+
 from .Logging import log
 
 class console:
+
     """コンソールの機能です(2)"""
-    pattern = [
-        {
-            "aliases": ["?"],
-            "output": [
-                "help - コマンドのヘルプを表示",
-                "stop - ボットを停止します。"
-            ],
-            "cmd": None,
-            "name": "help"
-        },
-        {
-            "aliases": ["exit", "alt+f4"],
-            "cmd": sys.exit,
-            "output": None,
-            "name": "stop"
-        }
-    ]
     
     @classmethod
     async def start(self):
         """コンソールを起動します。"""
+        db = dblib.manager()
+        cfg = config.loadConfig()
+
         log.info("コンソールを起動しました。")
         cs = await aioconsole.ainput("~ $ ")
 
         while True:
+            pattern = [
+                {
+                    "aliases": ["?"],
+                    "output": [
+                        "help - コマンドのヘルプを表示",
+                        "stop - ボットを停止します。"
+                        ],
+                    "cmd": None,
+                    "name": "help"
+                },
+                {
+                    "aliases": ["exit", "alt+f4"],
+                    "cmd": sys.exit,
+                    "output": None,
+                    "name": "stop"
+                },
+                {
+                    "aliases": ["stats"],
+                    "cmd": None,
+                    "output": [
+                        f"ALL MESSAGES: "+ str(len(db.get_contents(f'{cfg["database"]["prefix"]}message')))
+                    ],
+                    "name": "status"
+                }
+            ]
             runned = False
             log.info(f"コンソールコマンド \"{cs}\" を実行しました。")
-            for pt in self.pattern:
+            for pt in pattern:
                 if (pt["name"] == cs) or (cs in pt["aliases"]):
                     runned = True
                     if (not pt["output"] is None):
