60d862d282912b5c07e0760ea94ad41194dccc5a
Taitaitatata <taitaitatatasub@outlook.jp>
2022-03-22 21:37:18 +0900

    [Update] IndexErrorが出てしまう問題を修正

--- cogs/dashboard.py
+++ cogs/dashboard.py
@@ -17,29 +17,37 @@ class Dashboard(commands.Cog):
     @commands.slash_command(name="dashboard", description="ユーザーのダッシュボードを表示します。")
     async def cmd_dashboard(self, ctx: discord.ApplicationContext):
         await ctx.interaction.response.send_message(embed=discord.Embed(title="データベースにアクセス中...", color=discord.Color.green()), ephemeral=True)
-        
-        l = []
-        for msg in self.db.get_contents(f"{self.cfg['database']['prefix']}message", f"author_id = {ctx.author.id}"):
-            if (msg[0] == ctx.author.id):
-                l = l + [msg]
-        
-        i = []
-        for msg in self.db.get_contents(f"{self.cfg['database']['prefix']}message", f"author_id = {ctx.author.id} AND guild_id = {ctx.author.guild.id}"):
-            if (msg[0] == ctx.author.id and msg[3] == ctx.guild.id):
-                i = i + [msg]
 
-        s = []
-        for guild in self.db.get_contents(f"{self.cfg['database']['prefix']}message", f"author_id = {ctx.author.id}"):
-            s = s + [guild[3]]
-
-        guild_id = Counter(s).most_common()[0][0]
-        guild_message_count = Counter(s).most_common()[0][1]
-
-        g = self.bot.get_guild(guild_id)
-        
         embed = discord.Embed(title="ダッシュボード", color=discord.Color.green())
-        embed.add_field(name="メッセージ件数", value=f"{'{:,}'.format(len(i))}/{'{:,}'.format(len(l))}件")
-        embed.add_field(name="一番メッセージを送信するサーバー", value=f"{g.name} ({'{:,}'.format(guild_message_count)})")
+        
+        try:
+            l = []
+            for msg in self.db.get_contents(f"{self.cfg['database']['prefix']}message", f"author_id = {ctx.author.id}"):
+                if (msg[0] == ctx.author.id):
+                    l = l + [msg]
+        
+            i = []
+            for msg in self.db.get_contents(f"{self.cfg['database']['prefix']}message", f"author_id = {ctx.author.id} AND guild_id = {ctx.author.guild.id}"):
+                if (msg[0] == ctx.author.id and msg[3] == ctx.guild.id):
+                    i = i + [msg]
+        
+            embed.add_field(name="メッセージ件数", value=f"{'{:,}'.format(len(i))}/{'{:,}'.format(len(l))}件")
+        except:
+            pass
+
+        try:
+            s = []
+            for guild in self.db.get_contents(f"{self.cfg['database']['prefix']}message", f"author_id = {ctx.author.id}"):
+                s = s + [guild[2]]
+
+            guild_id = Counter(s).most_common()[0][0]
+            guild_message_count = Counter(s).most_common()[0][1]
+
+            g = self.bot.get_guild(guild_id)
+
+            embed.add_field(name="一番メッセージを送信するサーバー", value=f"{g.name} ({'{:,}'.format(guild_message_count)})")
+        except:
+            pass
 
         await ctx.edit(embed=embed)
 
