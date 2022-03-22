ad50a8d571d1b8afdbb9f4e5c1f193a2faa7fe0b
Taitaitatata <taitaitatatasub@outlook.jp>
2022-03-22 21:28:05 +0900

    [Update] ダッシュボードで一番メッセージを送ってる鯖取得できるようにに変更

--- cogs/dashboard.py
+++ cogs/dashboard.py
@@ -1,3 +1,4 @@
+from collections import Counter
 import discord, sys
 from discord.ext import commands
 
@@ -18,17 +19,27 @@ class Dashboard(commands.Cog):
         await ctx.interaction.response.send_message(embed=discord.Embed(title="データベースにアクセス中...", color=discord.Color.green()), ephemeral=True)
         
         l = []
-        for msg in self.db.get_contents(f"{self.cfg['database']['prefix']}message"):
+        for msg in self.db.get_contents(f"{self.cfg['database']['prefix']}message", f"author_id = {ctx.author.id}"):
             if (msg[0] == ctx.author.id):
                 l = l + [msg]
         
         i = []
-        for msg in self.db.get_contents(f"{self.cfg['database']['prefix']}message"):
+        for msg in self.db.get_contents(f"{self.cfg['database']['prefix']}message", f"author_id = {ctx.author.id} AND guild_id = {ctx.author.guild.id}"):
             if (msg[0] == ctx.author.id and msg[3] == ctx.guild.id):
                 i = i + [msg]
+
+        s = []
+        for guild in self.db.get_contents(f"{self.cfg['database']['prefix']}message", f"author_id = {ctx.author.id}"):
+            s = s + [guild[3]]
+
+        guild_id = Counter(s).most_common()[0][0]
+        guild_message_count = Counter(s).most_common()[0][1]
+
+        g = self.bot.get_guild(guild_id)
         
         embed = discord.Embed(title="ダッシュボード", color=discord.Color.green())
         embed.add_field(name="メッセージ件数", value=f"{'{:,}'.format(len(i))}/{'{:,}'.format(len(l))}件")
+        embed.add_field(name="一番メッセージを送信するサーバー", value=f"{g.name} ({'{:,}'.format(guild_message_count)})")
 
         await ctx.edit(embed=embed)
 
