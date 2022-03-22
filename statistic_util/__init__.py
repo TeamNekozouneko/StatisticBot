55f95ef8679bc400169eebfeae783ee4149b3d0b
Taitaitatata <taitaitatatasub@outlook.jp>
2022-03-22 21:30:28 +0900

    [Update] バージョン変更

--- statistic_util/__init__.py
+++ statistic_util/__init__.py
@@ -17,7 +17,7 @@ __copyright__ = "Copyright 2022 TEAM Nekozouneko"
 __license__ = "GNU General Public License v3"
 __repository__ = "https://github.com/TEAMNekozouneko/StatisticBot"
 __title__ = "Statistic Util"
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 def getVersion():
     return __version__
