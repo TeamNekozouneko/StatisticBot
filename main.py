fd2eccc931998a46beee4e85c58315175be1cdf6
Taitaitatata <taitaitatatasub@outlook.jp>
2022-03-24 12:11:09 +0900

    [Update] デバッグオプションを追加

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-import discord, os, sys, platform, traceback
+import argparse, discord, os, platform, statistic_util, sys, traceback
 
 from statistic_util import log, config
 
-print("ボットを起動中")
+parser = argparse.ArgumentParser(description='StatisticBotのコマンドパラメーターです。')
+
+parser.add_argument('-t', '--token', help='設定のトークンを無視して強制的にこのトークンを指定します。', type=str, default=None)
+parser.add_argument('--generate-config', help='設定ファイルを生成します。', action='store_true')
+parser.add_argument('--no-load-cogs', help='コグを読み込まないで実行します。', action='store_true')
+
+args = parser.parse_args()
+
+print("ボットを起動中...")
 print(f"Python: {sys.version} オペレーティングシステム: {platform.system()} {platform.version()}")
 
 intent = discord.Intents.default()
@@ -11,10 +19,13 @@ intent.messages=True
 intent.message_content=True
 intent.guild_messages=True
 
-bot = discord.Bot(intents=intent)
+bot = discord.Bot()
 
-if not os.path.exists("config.json"):
-    log.err("config.jsonが見つかりませんでした。生成しています...")
+if not os.path.exists("config.json") or args.generate_config:
+    if not args.generate_config:
+        log.err("config.jsonが見つかりませんでした。生成しています...")
+    else:
+        log.info("config.jsonを生成中...")
     config.saveDefaultConfig()
     log.warn("生成しました。config.jsonを確認してください。")
 
@@ -22,18 +33,22 @@ if not os.path.exists("config.json"):
 
 conf = config.loadConfig()
 
-for loadCogs in os.listdir("cogs/"):
-    loadf = os.path.splitext(loadCogs)
-    if loadf[1] == ".py":
-        log.info("{0}を読み込み中".format(loadf[0]+loadf[1]))
-        try:
-            bot.load_extension("cogs."+loadf[0])
-        except Exception as e:
-            log.warn("{0}は無効なコグファイルです。".format(loadf[0]+loadf[1]))
-            log.err(traceback.format_exception_only(type(e), e)[0].rstrip("\n"))
-            continue
+if not args.no_load_cogs:
+    for loadCogs in os.listdir("cogs/"):
+        loadf = os.path.splitext(loadCogs)
+        if loadf[1] == ".py":
+            log.info("{0}を読み込み中".format(loadf[0]+loadf[1]))
+            try:
+                bot.load_extension("cogs."+loadf[0])
+            except Exception as e:
+                log.warn("{0}は無効なコグファイルです。".format(loadf[0]+loadf[1]))
+                log.err(traceback.format_exception_only(type(e), e)[0].rstrip("\n"))
+                continue
 
 try:
-    bot.run(conf["token"])    
+    if args.token is None:
+        bot.run(conf["token"])    
+    else:
+        bot.run(args.token)
 except discord.errors.LoginFailure:
     log.err("トークンが無効です。config.jsonを確認してください。")
